18b22081a56329ec6e0f66628e307d0b45d520c6
acha-gamedev <acha.gamedev@gmail.com>
2020-10-16 22:15:50 -0400

    Built Randomizer

--- GameProductions/GameProductions/Randomizer.h
+++ GameProductions/GameProductions/Randomizer.h
@@ -1,11 +1,20 @@
 #ifndef RANDOMIZER_H
 #define RANDOMIZER_H
 
-#include <SDL_timer.h>
-
 class Randomizer
 {
+protected:
+	unsigned int rngValue;
+	unsigned int rngCount;
+public:
+	Randomizer(unsigned int seed);
+	~Randomizer();
+
+	void InitializeValue(unsigned int seed);
+	void AdvanceValue();
 	
+	int GetRandomIntegerRange(int lowerBound, int upperBound);
+	int GetRandomInteger(int range);
 };
 
 #endif
