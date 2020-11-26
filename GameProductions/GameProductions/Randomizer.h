dd2edf1426f83a5890c54a76cf455920ed046c2f
tienosca <tienoscar@hotmail.com>
2020-11-26 17:05:26 -0500

    Randomizer goal input added

--- GameProductions/GameProductions/Randomizer.h
+++ GameProductions/GameProductions/Randomizer.h
@@ -10,6 +10,10 @@ public:
 	Randomizer(unsigned int seed);
 	~Randomizer();
 
+	//goal is to link this randomizer class with projectile spawn, setup where initial loading of game will require input from player of how many lanes they
+	//want for endless generator, goal is to have randomzer spawn from 3-lane up to 5-lane via drag-down window user input
+
+
 	// resets the RNG with the given seed value
 	void InitializeValue(unsigned int seed);
 	// advances the RNG one step
