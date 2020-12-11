87eea7a1c6f206438760b4c10c8ffee962baff75
tienosca <tienoscar@hotmail.com>
2020-12-11 16:31:12 -0500

    removed un-needed code

--- GameProductions/GameProductions/ColliderComponent.h
+++ GameProductions/GameProductions/ColliderComponent.h
@@ -45,6 +45,7 @@ public:
 
 	void update() override
 	{
+		//sets collider box size for players
 		if (tag == "player")
 		{
 			collider.x = static_cast<int>(transform->position.x + 32);
@@ -53,6 +54,7 @@ public:
 			collider.h = ((transform->height * transform->scale) * 0.8);
 		}
 
+		//sets collider box size for projectiles
 		if (tag == "projectile")
 		{
 			collider.x = static_cast<int>(transform->position.x);
@@ -67,10 +69,10 @@ public:
 
 	void draw() override
 	{
-
-		//SDL_SetRenderDrawColor(Game::renderer, 0, 255, 0, 255);
-		//SDL_RenderDrawRect(Game::renderer, &collider);
+		//to draw the collision boxes for projectiles and player via green box 
+		SDL_SetRenderDrawColor(Game::renderer, 0, 255, 0, 255);
+		SDL_RenderDrawRect(Game::renderer, &collider);
 		TextureManager::Draw(tex, srcR, destR, SDL_FLIP_NONE);
-		//SDL_SetRenderDrawColor(Game::renderer, 255, 255, 255, 255);
+		SDL_SetRenderDrawColor(Game::renderer, 255, 255, 255, 255);
 	}
 };
--- GameProductions/GameProductions/Game.cpp
+++ GameProductions/GameProductions/Game.cpp
@@ -22,8 +22,6 @@ bool Game::isRunning = false;
 bool onGround = false;
 
 auto& player(manager.addEntity());
-//auto& label(manager.addEntity());
-
 
 Game::Game()
 {
@@ -54,22 +52,13 @@ void Game::init(const char* title, int width, int height, bool fullscreen)
 		isRunning = true;
 	}
 
-	/*
-	if (TTF_Init() == -1)
-	{
-		cout << "Error : SDL_TTF" << endl;
-	}
-	*/
-
+	//add textures for terrain (map), player, and projectile
 	assets->AddTexture("terrain", "Assets/terrain_ss.png");
 	assets->AddTexture("player", "Assets/player_anims.png");
 	assets->AddTexture("projectile", "Assets/proj.png");
 
-	//assets->AddFont("arial", "Assets/arial.ttf", 16);
-
 	maps = new Map("terrain", 2, 32); //map name, map scale size, tile size 
-
-	//maps->LoadMap("Assets/map.map", 25, 20);
+	maps->LoadMap("Assets/map.map", 25, 20);
 
 	player.addComponent<TransformComponent>(3); //scale the player size
 	player.addComponent<SpriteComponent>("player", true);
@@ -77,9 +66,6 @@ void Game::init(const char* title, int width, int height, bool fullscreen)
 	player.addComponent<ColliderComponent>("player"); //set tag as player
 	player.addGroup(groupPlayers);
 
-	//this if for UI labels, still a WIP ignore for now
-	//SDL_Color white = { 255, 255, 255, 255 };
-	//label.addComponent<UILabel>(10, 10, "Test String", "arial", white);
 
 	//this will create projectiles - first set of vector2d sets spawn position, then velocity, range and speed
 	assets->CreateProjectile(Vector2D(700, 315), Vector2D(-2, 0), 800, 2, "projectile"); //position of projectile
@@ -90,6 +76,7 @@ auto& tiles(manager.getGroup(Game::groupMap));
 auto& players(manager.getGroup(Game::groupPlayers));
 auto& colliders(manager.getGroup(Game::groupColliders));
 auto& projectiles(manager.getGroup(Game::groupProjectiles));
+
 
 void Game::handleEvents()
 {
@@ -110,61 +97,13 @@ void Game::update()
 {
 	SDL_Rect playerCol = player.getComponent<ColliderComponent>().collider;
 	Vector2D playerPos = player.getComponent<TransformComponent>().position;
-	Vector2D playerVel = player.getComponent<TransformComponent>().velocity;
-	Vector2D playerFor = player.getComponent<TransformComponent>().force;
-
-
 	player.getComponent<TransformComponent>().mass = 1.0f;
 
 	manager.refresh();
 	manager.update();
-
-	//player.getComponent<TransformComponent>().ApplyForce(Vector2D(0, 1)); //this would apply constant downwrds force - gravity
-
 	spawner->Update();
 
-	//if statements for collision
-	for (auto& c : colliders)
-	{
-		SDL_Rect cCol = c->getComponent<ColliderComponent>().collider;
-		if (Collision::AABB(cCol, playerCol))
-		{
-			cout << "player hit collider" << endl;
-			//player.getComponent<TransformComponent>().ApplyForce(Vector2D(0, 5));
-			//player.getComponent<TransformComponent>().acceleration.y = 0;
-			//player.getComponent<TransformComponent>().ApplyForce(Vector2D(0, -5));
-			//player.getComponent<TransformComponent>().ApplyForce(Vector2D(0, 0));
-			//player.getComponent<TransformComponent>().position.y = playerPos.y - 3;
-		} 
-
-		if (Collision::AABB(cCol, playerCol) && playerFor.x < 0)
-		{
-			cout << "player hit collider" << endl;
-			//player.getComponent<TransformComponent>().ApplyForce(Vector2D(0, 0));
-			player.getComponent<TransformComponent>().position.x = playerPos.x + 1;
-		}
-
-		if (Collision::AABB(cCol, playerCol) && playerVel.y == 0 && playerVel.x > 0)
-		{
-			cout << "player hit collider" << endl;
-			player.getComponent<TransformComponent>().position.x = playerPos.x - 1;
-		}
-
-		//else if (Collision::AABB(cCol, playerCol) && playerVel.x < 0 || playerVel.x > 0)
-	//	{
-			// << "player hit collider" << endl;
-				//playerVel.y = 0;
-				//playerVel.x = 0;
-				//player.getComponent<TransformComponent>().speed = 0;
-				//player.getComponent<TransformComponent>().position = playerPos;
-			//	player.getComponent<TransformComponent>().gravity = true;
-			//	player.getComponent<TransformComponent>().velocity.x = 0;
-				//player.getComponent<TransformComponent>().speed = 3;
-				//player.getComponent<TransformComponent>().gravity = true;
-	//	}
-		
-	}
-
+	//if statements for spawner collision
 	for (auto& p : projectiles)
 	{
 		if (Collision::AABB(player.getComponent<ColliderComponent>().collider, p->getComponent<ColliderComponent>().collider))
@@ -173,30 +112,6 @@ void Game::update()
 			p->destroy();
 		}
 	}
-
-	//to keep the camera following player in middle
-	//camera.x = player.getComponent<TransformComponent>().position.x - 400; //half of screen is 400 as map is 800 x 640
-	//camera.y = player.getComponent<TransformComponent>().position.y - 320;
-
-	if (camera.x < 0)
-	{
-		camera.x = 0;
-	}
-
-	if (camera.y < 0)
-	{
-		camera.y = 0;
-	}
-	
-	if (camera.x > camera.w)
-	{
-		camera.x = camera.w;
-	}
-
-	if (camera.y > camera.h)
-	{
-		camera.y = camera.h;
-	}
 }
 
 
@@ -204,29 +119,24 @@ void Game::render()
 {
 	SDL_RenderClear(renderer);
 
+	//render map
 	for (auto& t : tiles)
 	{
 		t->draw();
 	}
-
-	//render out if you dont want to see the collision box
-	for (auto& c : colliders)
-	{
-		c->draw();
-	}
 	
-
+	//render player
 	for (auto& p : players)
 	{
 		p->draw();
 	}
 
+	//render projectile
 	for (auto& p : projectiles)
 	{
 		p->draw();
 	}
 
-	//label.draw();
 	SDL_RenderPresent(renderer);
 }
 
